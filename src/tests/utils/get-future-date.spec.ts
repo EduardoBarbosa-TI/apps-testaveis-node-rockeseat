e2190364a77d20e415d93de46c5a4ad16f1f9095
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-06-14 19:48:29 -0300

    feat: added test for get-future-date utils

---
+++ src/tests/utils/get-future-date.spec.ts
@@ -0,0 +1,9 @@
+import { expect, it } from "vitest";
+import { getFutureDate } from "./get-future-date";
+
+it('increases date with one year', () => {
+
+    const year = new Date().getFullYear()
+
+    expect(getFutureDate(`${year}-06-14`).getFullYear()).toEqual(2024)
+})
