c51914c628f8514c81dab810726722ab723ddfb4
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-06-14 19:59:02 -0300

    style: removing white spaces in create-appointment use-cases

--- src/use-cases/create-appointment.ts
+++ src/use-cases/create-appointment.ts
@@ -8,33 +8,26 @@ interface CreateAppointmentRequest {
 }
 
 type CreateAppointmentResponse = Appointment
-
 export class CreateAppointment {
-    constructor(
-        private appointmentsRepository: AppointmentsRepository
-    ){
-
-    }
+    constructor(private appointmentsRepository: AppointmentsRepository){}
 
     async execute({ customer, startsAt, endsAt }: CreateAppointmentRequest): Promise<CreateAppointmentResponse> {
-
         const overlappingAppointment =  await this.appointmentsRepository.findOverlappingAppointment(
             startsAt,
             endsAt
         )
         
-        if(overlappingAppointment){
-           throw new Error('Another appointment overlaps this appointment dates') 
+        if(overlappingAppointment){ 
+            throw new Error('Another appointment overlaps this appointment dates') 
         }
 
         const appointment = new Appointment({
             customer,
             startsAt,
             endsAt
-    })
+        })
 
-    await this.appointmentsRepository.create(appointment)
-
+        await this.appointmentsRepository.create(appointment)
         return appointment
     }
 }
