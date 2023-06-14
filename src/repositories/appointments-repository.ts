5b613d5ab24ccea9eda2f054e73b04ac6700af02
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-06-14 19:57:27 -0300

    style: removing white spaces in appointment repository

--- src/repositories/appointments-repository.ts
+++ src/repositories/appointments-repository.ts
@@ -1,5 +1,4 @@
 import { Appointment } from "../entities/appointment";
-
 export interface AppointmentsRepository {
     create(appointment: Appointment): Promise<void>
     findOverlappingAppointment(startsAt: Date, endsAt: Date): Promise<Appointment | null> 
