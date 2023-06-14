85e8efa69d03b6d6ac9f555bf05324feb9d1d2f0
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-06-14 19:58:08 -0300

    style: removing white spaces get-future-date repository

--- src/tests/utils/get-future-date.spec.ts
+++ src/tests/utils/get-future-date.spec.ts
@@ -2,8 +2,6 @@ import { expect, it } from "vitest";
 import { getFutureDate } from "./get-future-date";
 
 it('increases date with one year', () => {
-
     const year = new Date().getFullYear()
-
     expect(getFutureDate(`${year}-06-14`).getFullYear()).toEqual(2024)
 })
